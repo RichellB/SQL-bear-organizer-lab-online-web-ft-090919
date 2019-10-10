f81a3da1aacf7213eaefbfe11ee5b4fab175e5e1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-10 10:48:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,16 @@
 INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Rowdy", 11, "M", "Yellow", "Mild", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Tabitha", 50, "F", "Purple", "Calm", 1);
+
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
