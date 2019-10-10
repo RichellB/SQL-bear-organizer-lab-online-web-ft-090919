9779479a92472e10025cac2cb217fd6c0b711e57
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-10 10:50:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,12 +5,12 @@ INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Rowdy",
 INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Tabitha", 50, "F", "Purple", "Calm", 1);
 
 
-INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Sargeant Brown", 35, "M", "Brown", "Calm", 0);
 
-INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Melissa", 22, "F", "Pink", "Happy", 1);
 
-INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Grinch", 78, "M", "Red", "Angry", 1);
 
-INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Wendy", 16, "F", "Orange", "Mild", 1);
 
 INSERT INTO bears(name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
