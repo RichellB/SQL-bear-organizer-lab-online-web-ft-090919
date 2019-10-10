c845fffe5f0e8c77903c909c5c7b04b309c43b69
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-10 10:55:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
+INSERT INTO bears (1, name, age, gender, color, temparament, alive) VALUES ("Mr. Chocolate", 20, "F", "Red", "Angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Rowdy", 11, "M", "Yellow", "Mild", 1);
+INSERT INTO bears (2, name, age, gender, color, temparament, alive) VALUES ("Rowdy", 11, "M", "Yellow", "Mild", 1);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Tabitha", 50, "F", "Purple", "Calm", 1);
+INSERT INTO bears (3, name, age, gender, color, temparament, alive) VALUES ("Tabitha", 50, "F", "Purple", "Calm", 1);
 
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Sargeant Brown", 35, "M", "Brown", "Calm", 0);
+INSERT INTO bears (4, name, age, gender, color, temparament, alive) VALUES ("Sargeant Brown", 35, "M", "Brown", "Calm", 0);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Melissa", 22, "F", "Pink", "Happy", 1);
+INSERT INTO bears (5, name, age, gender, color, temparament, alive) VALUES ("Melissa", 22, "F", "Pink", "Happy", 1);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Grinch", 78, "M", "Red", "Angry", 1);
+INSERT INTO bears (6, name, age, gender, color, temparament, alive) VALUES ("Grinch", 78, "M", "Red", "Angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ("Wendy", 16, "F", "Orange", "Mild", 1);
+INSERT INTO bears (7, name, age, gender, color, temparament, alive) VALUES ("Wendy", 16, "F", "Orange", "Mild", 1);
 
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES (NULL, 50, "M", "Red", "Angry", 0);
+INSERT INTO bears (8, name, age, gender, color, temparament, alive) VALUES (NULL, 50, "M", "Red", "Angry", 0);
