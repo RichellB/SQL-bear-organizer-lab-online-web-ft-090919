731635458f9d8991c440becc1f3bc59b71beccc7
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-10 10:57:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,15 +2,15 @@ INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "F", "Red", "Angry", 1);
 
 INSERT INTO bears VALUES (2, "Rowdy", 11, "M", "Yellow", "Mild", 1);
 
-INSERT INTO bears (3, name, age, gender, color, temparament, alive) VALUES ("Tabitha", 50, "F", "Purple", "Calm", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 50, "F", "Purple", "Calm", 1);
 
 
-INSERT INTO bears (4, name, age, gender, color, temparament, alive) VALUES ("Sargeant Brown", 35, "M", "Brown", "Calm", 0);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 35, "M", "Brown", "Calm", 0);
 
-INSERT INTO bears VALUES ("5, Melissa", 22, "F", "Pink", "Happy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 22, "F", "Pink", "Happy", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temparament, alive) VALUES ("Grinch", 78, "M", "Red", "Angry", 1);
+INSERT INTO bears VALUES (6, "Grinch", 78, "M", "Red", "Angry", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temparament, alive) VALUES ("Wendy", 16, "F", "Orange", "Mild", 1);
+INSERT INTO bears VALUES (7, "Wendy", 16, "F", "Orange", "Mild", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temparament, alive) VALUES (NULL, 50, "M", "Red", "Angry", 0);
+INSERT INTO bears VALUES (8, NULL, 50, "M", "Red", "Angry", 0);
